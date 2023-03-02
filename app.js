33192e224c99be74cfe445132ceb9e34cfbc8cc0
lenovalsha <lenovalsha@gmail.com>
2023-03-01 23:55:19 -0700

    work on backgroung image next

--- app.js
+++ app.js
@@ -6,37 +6,58 @@ const bodyParser = require("body-parser");
 const { rmSync } = require("fs");
 const { builtinModules } = require("module");
 const { TIMEOUT } = require("dns");
-// const { urlencoded } = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"))
-app.get("/",function(req,res){
-    
-    res.sendFile(__dirname +"/index.html" );
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public/images"))
+
+app.get("/", function (req, res) {
+
+    res.sendFile(__dirname + "/index.html");
 });
-app.post("/",function(req,res){
-   
+app.post("/", function (req, res) {
+
     const query = req.body.cityName;
-    const url = "https://api.openweathermap.org/data/2.5/weather?appid=b80569182e7018d858eb08834fd721b8&q=" +query+"&units=metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?appid=b80569182e7018d858eb08834fd721b8&q=" + query + "&units=metric";
     // res.send("Server is up and running");
-    https.get(url,function(response){
+    https.get(url, function (response) {
         console.log(response.statusCode);
-        response.on("data",function(data){
+        response.on("data", function (data) {
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
             const desc = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
             const weather = weatherData.weather[0].main;
+            const feelsLike = weatherData.main.feels_like;
+            const humidity = weatherData.main.humidity;
+            const windSpeed = weatherData.wind.speed;
+
             const imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
             console.log(weatherData);
-            
-            res.writeHead(200, {'Content-Type': 'text/html'});
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
             res.write(`<style type="text/css">
             body {
-              color: blue;
+              font-family: 'Garamond';
+              background-image: url("/public/images/bg.jpg");
+              background-size: cover;
+              background-repeat: no-repeat;
+              background-position: center;
+              width:100%;
+              height:100vh;
             }
             h1{
                 color:black;
+            }
+            p{
+                font-size:20px;
+            }
+            .city{
+                font-size:60px;
+            }
+            div{
+                text-align:center;
             }
             .container{
                 display:flex;
@@ -54,13 +75,16 @@ app.post("/",function(req,res){
 
                 width:50%;
             }
+            .weather{
+                font-size: 50px;
+            }
             .container2{
                 // background-Color:yellow;
                 display:flex;
-                justify-content:center;
+                justify-content:space-between;
                 align-items:center;
-                margin:15% 10%;
-                width:25%;
+                margin:5% auto 15%;
+                width:50%;
             }
             .border{
                 border-radius:25px;
@@ -72,19 +96,21 @@ app.post("/",function(req,res){
                 width:25%;
                 height:50px;
             }
+          
             </style>`);
-            res.write('<form action="/" method="post">'+
-            '<div class="container widthSearch"> <input class="border" type="text"name="cityName" placeholder="City Name"><button class="borderbtn" type="submit">Go</button></div>'+
-            '<div class="container2"><div><h1>'+query+'</h1><h1>' + temp + '</h1></div></div>'+
-            '<div class="container widthInfo"><h1>Feels like</h1><h1>Humidity</h1><h1>Windspeed</h1></div>'+
-            '</form>')
-            res.write("<p>"+weather+"</p>")
-            res.write("<img src="+ imgUrl+">");
-            res.send();
+            res.write(' <html>' +
+                '<body><form action="/" method="post">' +
+                '<div class="container widthSearch"> <input class="border" type="text" name="cityName" placeholder="City Name"><button class="borderbtn" type="submit">Go</button></div>' +
+                '<div class="container2"><div><i class="fas fa-coffee"></i><h1 class="city">' + query.toUpperCase() + '</h1><h1><i class="fa-solid fa-temperature-quarter"></i> ' + " " + temp + '</h1></div><div><img src=" ' + imgUrl + '" /><p class="weather">' + weather + '</p><p>' + desc.toUpperCase() + '</p></div></div>' +
+                '<div class="container widthInfo"><div><h1>FEELS LIKE</h1><i class="fa-solid fa-face-grimace"></i><h1>' + feelsLike + ' Celcius</h1></div><div><h1>HUMIDITY</h1><i class="fa-solid fa-droplet"></i><h1>' + humidity + '%</h1></div><div><h1>WINDSPEED</h1><i class="fa-solid fa-wind"></i><h1>' + windSpeed + ' m/s</h1></div></div>' +
+                '</form><script src="https://kit.fontawesome.com/a7c4c42cb0.js" crossorigin="anonymous"></script></body></html > ')
+
+            res.send(
+            );
         });
     });
 })
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("App is running on 3000");
-    
+
 })
